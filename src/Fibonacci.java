28894edb51c41d458ab9d50fc6f29e93c21448ee
Francis Oliver Malit <francisoliver.malit@gmail.com>
2023-04-11 14:44:35 +0800

    add fibonacci series with input 1 and 2 with seed

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -7,4 +7,26 @@ public class Fibonacci {
         return result(seed -1 ) + result(seed - 2);
 
     }
+
+    public static int series(int seed, int x, int y) {
+
+        if(seed == 1) {
+            return x;
+        }
+
+        if(seed == 2) {
+            return y;
+        }
+
+        int sum = x + y;
+
+        for(int index = 3; index < seed; index++) {
+            x = y;
+            y = sum;
+            sum = x + y;
+        }
+
+        return sum;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,14 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter the seed for our fibonacci");
         int seed = scanner.nextInt();
-        System.out.println("The fibonacci of " + seed + " is = "+ Fibonacci.result(seed));
+        //System.out.println("The fibonacci of " + seed + " is = "+ Fibonacci.result(seed));
+
+        System.out.println("Please enter the first number for our fibonacci series");
+        int x = scanner.nextInt();
+        System.out.println("Please enter the second number for our fibonacci series");
+        int y = scanner.nextInt();
+
+        System.out.println("The fibonacci series of seed " + seed + " with first number " + x +" and second number " + y +" is " + Fibonacci.series(seed, x , y));
+
     }
 }
