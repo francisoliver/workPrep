9448150cc14142d23fb247c0550606f9e9eb1a56
Francis Oliver Malit <francisoliver.malit@gmail.com>
2023-05-05 15:13:20 +0800

    sample code using format

---
+++ src/Practice/classSample/SampleUsingFormat.java
@@ -0,0 +1,17 @@
+package Practice.classSample;
+
+import java.util.Scanner;
+
+public class SampleUsingFormat {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the price $ ");
+        double price = scanner.nextDouble();
+        double discountedPrice = price * 0.85;
+        System.out.format("Your price is $%.2f after 15%% discount.\n", discountedPrice, price);
+
+    }
+
+}
