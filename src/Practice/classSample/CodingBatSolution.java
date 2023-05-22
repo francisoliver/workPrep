eb96292d0c5cd50756a37c362787561fa7ff8748
Francis Oliver Malit <francisoliver.malit@gmail.com>
2023-05-22 14:34:32 +0800

    solution for Map topping1

--- src/Practice/classSample/CodingBatSolution.java
+++ src/Practice/classSample/CodingBatSolution.java
@@ -168,6 +168,15 @@ public class CodingBatSolution {
         return nums;
     }
 
+    public Map<String, String> topping1(Map<String, String> map) {
+        if(map.containsKey("ice cream")) {
+            map.put("ice cream", "cherry");
+        }
+        map.put("bread", "butter");
+        return map;
+    }
+
+
 
 
 }
