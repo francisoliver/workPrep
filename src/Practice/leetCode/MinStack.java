2f4f7a1f2ca5f644e9bf8e88449afdc5d0fe9db6
Francis Oliver Malit <francisoliver.malit@gmail.com>
2023-07-27 15:14:45 +0800

    minStack class solution

---
+++ src/Practice/leetCode/MinStack.java
@@ -0,0 +1,36 @@
+package Practice.leetCode;
+
+import java.util.Stack;
+
+class MinStack {
+
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+
+        stack.push(val);
+        minStack.push(Math.min(val, minStack.isEmpty() ? val : minStack.peek()));
+
+    }
+
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+
+}
