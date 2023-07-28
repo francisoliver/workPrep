98827f7df9d7cdf3169b9d1f5408d9a86c1c7fe4
Francis Oliver Malit <francisoliver.malit@gmail.com>
2023-07-28 08:54:52 +0800

    solution daily temp

--- src/Practice/leetCode/Solution.java
+++ src/Practice/leetCode/Solution.java
@@ -30,6 +30,19 @@ public class Solution {
         System.out.println("hello world!");
 
     }
+
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] ans = new int[temperatures.length];
+        Stack<Integer> stack = new Stack<>();
+        for (int currDay = 0; currDay < temperatures.length; currDay++) {
+            while (!stack.isEmpty() && temperatures[currDay] > temperatures[stack.peek()]) {
+                int prevDay = stack.pop();
+                ans[prevDay] = currDay - prevDay;
+            }
+            stack.add(currDay);
+        }
+        return ans;
+    }
     public static int evalRPN(String[] tokens) {
 
         if (tokens.length == 1) return Integer.parseInt(tokens[0]);
