261b028fe032a24007e97b6507f272a491fe8753
Francis Oliver Malit <francisoliver.malit@gmail.com>
2023-08-18 07:51:39 +0800

    my first DFS

--- src/Practice/leetCode/trees/Solution.java
+++ src/Practice/leetCode/trees/Solution.java
@@ -63,5 +63,22 @@ public class Solution {
         }
         return count;
     }
+    int maxTreeNode = -1;
+    public int diameterOfBinaryTree(TreeNode root) {
+        dfs(root);
+        return maxTreeNode;
+    }
+
+    private int dfs(TreeNode root) {
+
+        int left =0, right = 0;
+
+        if(root.right != null) right = dfs(root.right);
+        if(root.left != null) left = dfs(root.left);
+
+        maxTreeNode = Math.max(maxTreeNode, left + right);
+        return Math.max(left, right) +1;
+
+    }
 
 }
