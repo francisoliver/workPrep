784852f5a235b7aba4c501cc433948fc18346688
Francis Oliver Malit <francisoliver.malit@gmail.com>
2023-09-06 16:05:19 +0800

    solution for lowestCommonAncestor

--- src/Practice/leetCode/trees/Solution.java
+++ src/Practice/leetCode/trees/Solution.java
@@ -152,4 +152,14 @@ public class Solution {
         return false;
     }
 
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        if(p.val > root.val  && q.val > root.val)
+            return lowestCommonAncestor(root.right, p, q);
+        if(p.val < root.val && q.val < root.val)
+            return lowestCommonAncestor(root.left, p, q);
+        return root;
+
+    }
+
 }
