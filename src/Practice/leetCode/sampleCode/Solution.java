a4a86de05aaa7a7339529106f20bf2730a8bb1c6
Francis Oliver Malit <francisoliver.malit@gmail.com>
2023-09-21 13:49:13 +0800

    solution for containsNearby

--- src/Practice/leetCode/sampleCode/Solution.java
+++ src/Practice/leetCode/sampleCode/Solution.java
@@ -1,6 +1,7 @@
 package Practice.leetCode.sampleCode;
 
 import java.util.HashMap;
+import java.util.TreeSet;
 
 public class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
@@ -17,6 +18,34 @@ public class Solution {
             map.put(n, i);
         }
         return false;
+
+    }
+
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int indexDiff, int valueDiff) {
+
+        if (nums.length <= 1 || nums == null) return false;
+
+        TreeSet<Integer> set = new TreeSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+
+            Integer floor = set.floor(num + valueDiff);
+            Integer ceiling = set.ceiling(num - valueDiff);
+
+            if (floor != null && floor >= num || ceiling != null && ceiling <= num) {
+                return true;
+            }
+
+            set.add(num);
+
+            if (i >= indexDiff) {
+                set.remove(nums[i - indexDiff]);
+            }
+        }
+
+        return false;
+
 
     }
 }
